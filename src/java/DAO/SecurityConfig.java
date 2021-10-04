33d18fb57ce8d74e56029fd218f837f81c1b55d7
viethmhe150098 <viethmhe150098@fpt.edu.vn>
2021-10-04 15:12:42 +0700

    update auth

--- src/java/DAO/SecurityConfig.java
+++ src/java/DAO/SecurityConfig.java
@@ -5,11 +5,40 @@
  */
 package DAO;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 /**
  *
  * @author Viet
  */
+//SecurityConfig giúp cấu hình các vai trò và các chức năng (các trang) được phép truy cập ứng với vai trò đó.
 public class SecurityConfig {
-    public static final ROLE_MANAGER = "MANAGER";
-    public static final ROLE_EMPLOYEE = "EMPLOYEE";
+
+    public static final String ROLE_MANAGER = "2";
+    public static final String ROLE_CUSTOMER = "4";
+    public static final String ROLE_ADMIN = "1";
+    public static final String ROLE_STAFF = "3";
+    private static final Map<String, List<String>> mapConfig = new HashMap<String, List<String>>();
+
+    private static void init() {
+        List<String> urlPattern1 = new ArrayList<String>();
+        urlPattern1.add("/userinfo");
+        urlPattern1.add("/homepage");
+        urlPattern1.add("/register");
+        mapConfig.put(ROLE_CUSTOMER, urlPattern1);
+
+        List<String> urlPattern2 = new ArrayList<String>();
+    }
+
+    public static Set<String> getAllAppRoles() {
+        return mapConfig.keySet();
+    }
+
+    public static List<String> getUrlPatternsForRole(String role) {
+        return mapConfig.get(role);
+    }
 }
