e3abc19769ff83b7877e906e3b4cfc4f795fdc5d
haindhe141482 <90599135+haindhe141482@users.noreply.github.com>
2021-10-04 13:12:21 +0700

    Update Entity (Post)
    complete entity Post

--- src/java/Entity/Post.java
+++ src/java/Entity/Post.java
@@ -5,10 +5,89 @@
  */
 package Entity;
 
+import java.util.Date;
+
 /**
  *
  * @author Viet
  */
 public class Post {
+    private String title;
+    private int mID;
+    private Date date_create;
+    private Date updata_date;
+    private String status;
+    private int pcateID;
+    private int pID;
+
+    public Post() {
+    }
+
+    public Post(String title, int mID, Date date_create, Date updata_date, String status, int pcateID, int pID) {
+        this.title = title;
+        this.mID = mID;
+        this.date_create = date_create;
+        this.updata_date = updata_date;
+        this.status = status;
+        this.pcateID = pcateID;
+        this.pID = pID;
+    }
+
+    public int getpID() {
+        return pID;
+    }
+
+    public void setpID(int pID) {
+        this.pID = pID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getmID() {
+        return mID;
+    }
+
+    public void setmID(int mID) {
+        this.mID = mID;
+    }
+
+    public Date getDate_create() {
+        return date_create;
+    }
+
+    public void setDate_create(Date date_create) {
+        this.date_create = date_create;
+    }
+
+    public Date getUpdata_date() {
+        return updata_date;
+    }
+
+    public void setUpdata_date(Date updata_date) {
+        this.updata_date = updata_date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public int getPcateID() {
+        return pcateID;
+    }
+
+    public void setPcateID(int pcateID) {
+        this.pcateID = pcateID;
+    }
+    
     
 }
